2aed4a031c85dff2f33a7c306811679086b4b891
Ian Macharia <143433297+I-Macharia@users.noreply.github.com>
2024-06-03 15:36:48 +0300

    Update chatbot.py
    spacy en core module

--- chatbot.py
+++ chatbot.py
@@ -8,9 +8,10 @@ from sklearn.utils import resample
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
 from my_functions import load_results, QuoteFinder
-from spacy.cli import download
+from spacy.cli 
 
-download("en_core_web_sm")
+# Download the model using spacy.cli.download
+spacy.cli.download("en_core_web_sm")
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
