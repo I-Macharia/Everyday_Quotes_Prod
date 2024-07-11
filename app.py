a825121b8e19a886fbea2283e9bb29ae7ed8aac3
Ian Macharia <143433297+I-Macharia@users.noreply.github.com>
2024-07-12 02:56:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ import spacy
 import spacy.cli
 
 # Download the model using spacy.cli.download
-spacy.cli.download("en_core_web_sm")
+#spacy.cli.download("en_core_web_sm")
 
-nltk.download("punkt")
-nltk.download("stopwords")
+#nltk.download("punkt")
+#nltk.download("stopwords")
 
 
 # Load spaCy model
